256521109428ebbbca6bcde28205f033a80cf5cd
Rene Donkers <rene@lotsofpixels.nl>
2021-07-04 10:56:50 +0200

    Inital commit

---
+++ src/registration.php
@@ -0,0 +1,7 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'LotsofPixels_BottomDescription',
+    __DIR__
+);
+
